04be400b6dba8c25aa3189d9b8eae1541418e66f
Darlite <warmyneptune@gmail.com>
2017-03-21 21:56:57 +0300

    Fixed tabs

--- calculator.js
+++ calculator.js
@@ -3,17 +3,17 @@ function add (a, b) {
 }
 
 function sub(a, b) {
-	return a - b;
+  return a - b;
 }
 
 function multiplication(a,b) {
-	return a * b;
+  return a * b;
 }
 
 function division (a,b) {
-	return a / b;
+  return a / b;
 }
 
 function intDivision (a,b) {
-	return a % b;
+  return a % b;
 }
