93981dd3ee34b8c9247c52f17ac329a4467d41da
Darlite <warmyneptune@gmail.com>
2017-03-22 08:58:11 +0300

    Changed number of spaces from 4 to 2

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,21 +1,21 @@
 function add (a, b) {
-    return a + b;
+  return a + b;
 }
 
 function sub(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiplication(a,b) {
-    return a * b;
+  return a * b;
 }
 
 function division (a,b) {
-    return a / b;
+  return a / b;
 }
 
 function intDivision (a,b) {
-    return parseInt(a / b);
+  return parseInt(a / b);
 }
 
 console.log("Add function: ");
