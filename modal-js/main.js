20b0bf5ea541db77716c1656694213e698695abe
Darlite <warmyneptune@gmail.com>
2017-04-05 14:17:02 +0300

    Changed functions to match general solution

--- modal-js/main.js
+++ modal-js/main.js
@@ -1,19 +1,9 @@
-var modal = document.getElementById("single-modal");
-
-var openButton = document.getElementById("open-modal-button");
-
-var closeModal = document.getElementsByClassName("close-modal")[0];
-
-openButton.onclick = function() {
+function displayModal(id) {
+  var modal = document.getElementById(id);
   modal.style.display = "block";
 }
 
-closeModal.onclick = function() {
+function closeModal(id) {
+  var modal = document.getElementById(id);
   modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
