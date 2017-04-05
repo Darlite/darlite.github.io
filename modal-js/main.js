04914b561c5f8b20b4c2df6ba6dd418be0301980
Darlite <warmyneptune@gmail.com>
2017-04-05 14:29:30 +0300

    Fixed copypaste in functions

--- modal-js/main.js
+++ modal-js/main.js
@@ -1,9 +1,8 @@
-function displayModal(id) {
+function changeModalDisplay(id, t) {
   var modal = document.getElementById(id);
-  modal.style.display = "block";
-}
-
-function closeModal(id) {
-  var modal = document.getElementById(id);
-  modal.style.display = "none";
+  if (t.id == 'open-modal-button') {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
 }
